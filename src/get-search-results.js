5aa9c870993b0f19ad4bda6a76cb9cbd6a9510f7
Richie <rcatwr@users.noreply.github.com>
2020-08-19 16:16:35 -0700

    Update get-search-results.js
    removed api key

--- src/get-search-results.js
+++ src/get-search-results.js
@@ -1,5 +1,8 @@
+
+const key = "your api key";
+
 export default async search => {
-    const result = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=BFBBa4f0yniZOsn9By2Zul74hii2WBtq&q=${search}&limit=25&offset=0&rating=g&lang=en`);
+    const result = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${key}${search}&limit=25&offset=0&rating=g&lang=en`);
     const searchResults = await result.json();
     //console.log(searchResults);
     const images = searchResults.data.map(item => {
